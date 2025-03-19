b81de0f486a39b7b1060dc3d7146f3d2f4b7badb
rajibulislam60 <rajibulislam60707877@gmail.com>
2025-03-19 23:56:42 +0600

    payment gate way is bug

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -38,10 +38,11 @@ async function addOrderController(req, res) {
       data: order,
     });
   } else {
+    const transactionId = `txn_${Date.now()}`;
     const data = {
-      total_amount: 100,
+      total_amount: totalprice,
       currency: "BDT",
-      tran_id: "REF123", // use unique tran_id for each api call
+      tran_id: transactionId,
       success_url: "http://localhost:3030/success",
       fail_url: "http://localhost:3030/fail",
       cancel_url: "http://localhost:3030/cancel",
@@ -50,15 +51,15 @@ async function addOrderController(req, res) {
       product_name: "Computer.",
       product_category: "Electronic",
       product_profile: "general",
-      cus_name: "Customer Name",
-      cus_email: "customer@example.com",
-      cus_add1: "Dhaka",
-      cus_add2: "Dhaka",
+      cus_name: user.name,
+      cus_email: user.email,
+      cus_add1: city,
+      cus_add2: address,
       cus_city: "Dhaka",
       cus_state: "Dhaka",
       cus_postcode: "1000",
       cus_country: "Bangladesh",
-      cus_phone: "01711111111",
+      cus_phone: phone,
       cus_fax: "01711111111",
       ship_name: "Customer Name",
       ship_add1: "Dhaka",
@@ -68,12 +69,12 @@ async function addOrderController(req, res) {
       ship_postcode: 1000,
       ship_country: "Bangladesh",
     };
-    const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live)
-    sslcz.init(data).then(apiResponse => {
-        // Redirect the user to payment gateway
-        let GatewayPageURL = apiResponse.GatewayPageURL
-        res.redirect(GatewayPageURL)
-        console.log('Redirecting to: ', GatewayPageURL)
+    const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live);
+    sslcz.init(data).then((apiResponse) => {
+      // Redirect the user to payment gateway
+        let GatewayPageURL = apiResponse.GatewayPageURL;
+      res.redirect(GatewayPageURL);
+      console.log("Redirecting to: ", GatewayPageURL);
     });
   }
 }
